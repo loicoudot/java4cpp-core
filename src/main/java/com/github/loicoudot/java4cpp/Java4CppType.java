958727a901efdf59d2036d38866538ea34a49b39
Loïc Oudot <loic.oudot@gmail.com>
2014-01-14 23:34:17 +0100

    javadoc

--- src/main/java/com/github/loicoudot/java4cpp/Java4CppType.java
+++ src/main/java/com/github/loicoudot/java4cpp/Java4CppType.java
@@ -9,13 +9,45 @@ import java.lang.reflect.TypeVariable;
 import java.lang.reflect.WildcardType;
 import java.util.List;
 
+/**
+ * Reprensent types inside java4cpp.
+ * 
+ * Difference between <code>Java4cppType</code> and <code>Type</code>:
+ * <ul>
+ * <li>pure class, no interfaces</li>
+ * <li>Wildcard are replaced by <code>Object</code> (<code>List&lt;?></code>
+ * will be replaced by <code>List&lt;Object></code>)
+ * <li>Generic are replaced by upper bounds type (
+ * <code>List&lt;E extends String></code> will be replaced by
+ * <code>List&lt;String></code>)
+ * </ul>
+ * 
+ * Each different <code>Java4cppType</code> is a singleton, so it can be used as
+ * a key.
+ * 
+ * @author Loic Oudot
+ * 
+ */
 public class Java4CppType {
 
-    private Class<?> rawClass;
-    private List<Java4CppType> parameterizedTypes = newArrayList();
+    private final Class<?> rawClass;
+    private final List<Java4CppType> parameterizedTypes = newArrayList();
 
     private static List<Java4CppType> typeCache = newArrayList();
 
+    /**
+     * Returns the corresponding <code>Java4CppType</code> of a
+     * <code>Type</code>.
+     * 
+     * Even if <code>List&lt;E></code> and <code>List&lt;?></code> are two
+     * different instance of <code>Type</code>, this method returns the same
+     * instance of <code>Java4CppType</code> representing
+     * <code>List&lt;Object></code>.
+     * 
+     * @param type
+     *            a Java <code>Type</code>
+     * @return the corresponding <code>Java4CppType</code> singleton
+     */
     public static Java4CppType fromType(Type type) {
         Java4CppType j4cType = new Java4CppType(type);
         int index = typeCache.indexOf(j4cType);
@@ -37,10 +69,24 @@ public class Java4CppType {
         }
     }
 
+    /**
+     * The base raw class of a type i.e. <code>List</code> for
+     * <code>List&lt;?></code>
+     * 
+     * @return base raw class
+     */
     public Class<?> getRawClass() {
         return rawClass;
     }
 
+    /**
+     * For parameterized type, contains an ordered list of
+     * <code>Java4CppType</code> type compositing the type i.e. for a
+     * <code>Map&ltInteger, String></code> returns a list with
+     * <code>{ Integer, String }</code>.
+     * 
+     * @return list of type arguments
+     */
     public List<Java4CppType> getParameterizedTypes() {
         return parameterizedTypes;
     }
@@ -51,7 +97,7 @@ public class Java4CppType {
      * <li>Inner class for arrays</li>
      * <li>Non parameterized class for parameterized type</li>
      * <li>Upper bounds class for type variable</li>
-     * <ul>
+     * </ul>
      * 
      * @param type
      *            java type
